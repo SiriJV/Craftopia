2448b269b178749fe49c7959cf7a7d98d6e1ae51
SiriJV <siri.viren@icloud.com>
2025-01-30 10:21:52 +0100

    Added games setup

---
+++ src/minigames/games.ts
@@ -0,0 +1,7 @@
+const games = [
+    { name: "Enderman Evasion", icon: "enderman-icon.png", description: "Avoid looking the Enderman in the eyes!" },
+    { name: "Biome Blunder", icon: "sapling", description: "Guess the biome based on a blurry image!" },
+    { name: "Recipe Roulette", icon: "fence", description: "Guess the crafting recipe!"},
+    { name: "Mining Memory", icon: "emerald", description: "Classic memory!"},
+    { name: "Diamond Dig", icon: "picaxe", description: "Find the diamonds and avoid the pufferfish!"}
+  ];
